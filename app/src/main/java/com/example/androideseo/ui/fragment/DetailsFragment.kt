172a758261caa58e823ce97d3621243f2b5d1655
Mcourier <martincourierdemere@gmail.com>
2021-05-12 14:25:03 +0200

    Réorganisation fichiers 11/05/2021

--- app/src/main/java/com/example/androideseo/ui/fragment/DetailsFragment.kt
+++ app/src/main/java/com/example/androideseo/ui/fragment/DetailsFragment.kt
@@ -17,7 +17,11 @@ import com.afollestad.recyclical.datasource.emptyDataSource
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
 import com.example.androideseo.R
+import com.example.androideseo.data.LocalPreferences
+import com.example.androideseo.databinding.ActivityConnexionBinding
+import com.example.androideseo.databinding.ActivitySensorBinding
 import com.example.androideseo.service.ServiceClient
+import com.example.androideseo.ui.app.MainActivity
 
 
 import kotlinx.coroutines.CoroutineScope
@@ -28,14 +32,22 @@ class DetailsFragment : Fragment() {
 
     val args: DetailsFragmentArgs by navArgs()
 
+    private lateinit var binding: ActivityDetailBinding// <-- Référence à notre ViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySensorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false)
@@ -53,6 +65,22 @@ class DetailsFragment : Fragment() {
                 view?.apply {
                     val identity = findViewById<TextView>(R.id.identity)
                     identity.text = user.identity()
+
+                    binding.testdelete?.setOnClickListener {
+                        val user_name = user_name.text;
+                        val password = password.text;
+                        CoroutineScope(Dispatchers.IO).launch {
+                            runCatching {
+                                val res = ServiceClient.instance.connexion(user_name,password)
+                                LocalPreferences.getInstance(this@ConnexionActivity).addTokenToHistory(res.token)
+                                runOnUiThread{
+                                    Toast.makeText(this@ConnexionActivity, "Vous etes connecté",
+                                            Toast.LENGTH_SHORT).show()
+                                    startActivity(MainActivity.getStartIntent(this@ConnexionActivity))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
